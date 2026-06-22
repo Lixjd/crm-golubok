3ef6a8a6b12227100ecd0314238722ae935dc496
Lixjd <71213713+Lixjd@users.noreply.github.com>
2026-06-23 02:34:54 +0300

    Delete data.js

--- data.js
+++
@@ -1,9 +0,0 @@
-let db = JSON.parse(localStorage.getItem('crmData')) || {
-    employees: [],
-    roles: ['Куратор', 'Инструктор', 'Стажер'],
-    warnings: []
-};
-
-function saveDb() {
-    localStorage.setItem('crmData', JSON.stringify(db));
-}
