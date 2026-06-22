74344f6bedc2b83ab94ed677b527d0844894b69b
Lixjd <71213713+Lixjd@users.noreply.github.com>
2026-06-23 02:21:39 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,7 +1,4 @@
-// Загрузка данных или создание пустой базы
-let db = JSON.parse(localStorage.getItem('crmData')) || {
-    employees: []
-};
+let db = JSON.parse(localStorage.getItem('crmData')) || { employees: [] };
 
 function saveDb() {
     localStorage.setItem('crmData', JSON.stringify(db));
