25046ea454831aa9a58c9ecab94907f29d333795
Lixjd <71213713+Lixjd@users.noreply.github.com>
2026-06-23 02:24:58 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,4 +1,9 @@
-let db = JSON.parse(localStorage.getItem('crmData')) || { employees: [] };
+let db = JSON.parse(localStorage.getItem('crmData')) || {
+    employees: [],
+    roles: ['Куратор', 'Инструктор', 'Стажер'],
+    tasks: ['Патруль', 'Проверка', 'Собеседование'],
+    warnings: []
+};
 
 function saveDb() {
     localStorage.setItem('crmData', JSON.stringify(db));
